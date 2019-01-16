516c04ced0bf2a798c60102ed1c21e96294a419c
gmastrbit <gmastrbit@gmail.com>
2019-01-16 19:25:26 +0300

    add first game

---
+++ app/src/main/java/com/example/nensy/memgame/ListActivity.java
@@ -0,0 +1,24 @@
+package com.example.nensy.memgame;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        // приховує статус бар:
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // приховує ActionBar:
+        getSupportActionBar().hide();
+        // орієнтація екрана:
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+
+}
--- app/src/main/java/com/example/nensy/memgame/MainActivity.java
+++ app/src/main/java/com/example/nensy/memgame/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.nensy.memgame;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // rjghuerg
+        // приховує статус бар:
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // приховує ActionBar:
+        getSupportActionBar().hide();
+        // орієнтація екрана:
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void showRules(View view){
+        // перехід на головну з анімацією:
+        Intent questionIntent = new Intent(MainActivity.this, RulesActivity.class);
+        startActivityForResult(questionIntent, 1);
+        overridePendingTransition(R.anim.right_in, R.anim.left_out);
+        //finish();
+    }
+
+    public void showList(View view){
+        // перехід на головну з анімацією:
+        Intent questionIntent = new Intent(MainActivity.this, ListActivity.class);
+        startActivityForResult(questionIntent, 1);
+        overridePendingTransition(R.anim.right_in, R.anim.left_out);
+        //finish();
     }
 }
