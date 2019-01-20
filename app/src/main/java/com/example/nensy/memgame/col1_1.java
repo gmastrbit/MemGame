a8619d2d0e4f629e946237108230bb83a10f122c
gmastrbit <gmastrbit@gmail.com>
2019-01-20 14:36:04 +0300

    volume 4 game 1
    errors (3 volume) not done

---
+++ app/src/main/java/com/example/nensy/memgame/col1_1.java
@@ -0,0 +1,13 @@
+package com.example.nensy.memgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class col1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.col1_1);
+    }
+}
